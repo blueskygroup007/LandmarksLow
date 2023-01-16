b2b63d62dda52f5aaa981153907e3eecf3ebeb48
officemacos <officemacos@123.com>
2023-01-16 14:06:27 +0800

    Initial Commit

---
+++ LandmarksLow/LandmarksLowApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksLowApp.swift
+//  LandmarksLow
+//
+//  Created by blueskygroup on 2023/1/16.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksLowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
